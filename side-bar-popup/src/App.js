e3f2a7109c7718bc70cb96ef9ddfb5dd5b96187d
Mario Fernandez <diddleslip@gmail.com>
2020-09-17 18:56:27 -0400

    Made Carousel appear through PopUpBar instead of this file

--- side-bar-popup/src/App.js
+++ side-bar-popup/src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import './App.css';
 import PopUpBar from "./components/PopUp/PopUpBar";
-import Carousel from "./components/Carousel/Carousel"
 
 function App() {
   return (
     <div className="App">
-      <Carousel/>
       <PopUpBar/>
-      <img src="https://i.imgur.com/VTpz8XB.jpg"/>
     </div>
   );
 }
