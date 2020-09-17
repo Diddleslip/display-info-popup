1519ffb6ab420b876cf1e859678812a28365681a
Mario Fernandez <diddleslip@gmail.com>
2020-09-16 21:41:41 -0400

    Initialized dummy data

---
+++ side-bar-popup/src/components/PopUpBar/PopUpData.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export const PopUpData = {
+  "records": [
+    {
+      "id": "RecordId1",
+      "name": "Bradford Soap Works",
+      "type": {
+        "name": "Manufacturing Partners"
+      },
+      "coordinates": {
+        "latitude": 41.715078,
+        "longitude": -71.510541
+      },
+      "fields": [
+        {
+          "name": "Website",
+          "value": "http://www.bradfordsoap.com/"
+        },
+      ]
+    },
+  ]
+}
